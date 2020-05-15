fc23152813a0d07cdf132c458d4f8206433199cf
lpablo <gevolutionxalapa>
2020-05-15 00:28:41 -0500

    Definiendo los activities and fragments de la aplicación

---
+++ app/src/main/java/com/example/plazitconfpractice/ui/activities/SplashScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.example.plazitconfpractice.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.plazitconfpractice.R
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
