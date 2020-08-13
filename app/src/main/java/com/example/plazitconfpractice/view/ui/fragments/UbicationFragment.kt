65579ef4d746d163baafcc481f55b6434b809d45
lpablo <gevolutionxalapa>
2020-08-13 09:06:29 -0500

    Configuracion del mapa en el frameng y en su actividad

--- app/src/main/java/com/example/plazitconfpractice/view/ui/fragments/UbicationFragment.kt
+++ app/src/main/java/com/example/plazitconfpractice/view/ui/fragments/UbicationFragment.kt
@@ -7,18 +7,31 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.example.plazitconfpractice.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
 /**
  * A simple [Fragment] subclass.
  */
-class UbicationFragment : Fragment() {
+class UbicationFragment : Fragment(), OnMapReadyCallback {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_ubication, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(p0: GoogleMap?) {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        /*val UPV = LatLng(39.481106, -0.340987) //Nos ubicamos en la UPV
+        googleMap.addMarker(MarkerOptions().position(UPV).title("Marker UPV"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(UPV))*/
     }
 
 }
